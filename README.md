a54a25e8fd813ab0a706f4558efe10dbbb25c5d5
sabrinamsantos <desenvolvedora.sabrina@outlook.com>
2023-10-03 21:41:44 -0300

    deploy project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# calculateAverageOfTwoValues
+# calculateAverageOfTwoValues - Documentação
  
+## Descrição
+
+Função que calcula a média de dois valores passados como parâmetro
+
+## Parâmetros
+
+- n1: Primeiro valor
+- n2: Segundo valor
+
+## Retorno
+
+- Média dos valores como parâmetro
+
+## Exemplo em C#
+
+```csharp
+var media = Media(10, 5)
+```
+
+## Autor
+
+- Sabrina Santos
---
+++ calculator.cs
@@ -0,0 +1,17 @@
+<summary>
+// Função que calcula a média dos valores passados como parâmetro
+<param name="n1">Primeiro Valor</param>
+<param name="n2">Segundo Valor</param>
+<returns>Média dos valores</returns>
+</summary>
+
+namespace calculator
+{
+    public class calcula
+    {
+        public double Media(double n1, double n2)
+        {
+            return (n1 + n2) /2;
+        }
+    }
+}
